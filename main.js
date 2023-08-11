ac664bf97ea19562c596511b3c4f2bf1d76e8942
Kashish Kumar <kashishkumar.pc@gmail.com>
2023-08-11 11:32:53 +1000

    Created Field class and added print() functionality

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+
+    print() {
+        let fieldPrint = this.field.map((rowArray) => rowArray.join(''));
+        console.log(fieldPrint.join('\n'));
+    }
+};
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+  
+//myField.print();
