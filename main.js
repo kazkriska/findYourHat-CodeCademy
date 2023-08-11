d29cd66016779c663ac07ccc5b9a8b602c9bad59
Kashish Kumar <kashishkumar.pc@gmail.com>
2023-08-11 18:01:16 +1000

    Field Boundary logic implemented

--- main.js
+++ main.js
@@ -11,12 +11,22 @@ class Field {
         this.field = field;
         this.row = 0;
         this.col = 0;
-        this.startingPosition = [0,0];
+        this.currentPosition = [0,0];
     }
 
-    print(str) {
+    print() {
         let fieldPrint = this.field.map((rowArray) => rowArray.join(''));
         console.log(fieldPrint.join('\n'));
+    }
+
+    isMoveWithinBoundary(move) {
+        let positionOffset = inputLogic(move);
+        let nextPosition = [this.currentPosition[0] + positionOffset[0], this.currentPosition[1] + positionOffset[1]];
+        if (nextPosition[0] >= 0 && nextPosition[0] < this.field.length && nextPosition[1] >= 0 && nextPosition[1] < this.field[0].length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
@@ -26,4 +36,16 @@ const myField = new Field([
     ['░', '^', '░'],
   ]);
   
-myField.print();
+
+let isNextMovePossible = true;
+while (isNextMovePossible) {
+    myField.print();
+    let move = prompt('Which way? ');
+    if (myField.isMoveWithinBoundary(move)) {
+        console.clear();
+        isNextMovePossible = true;
+    } else {
+        console.log('You went out of the field, GAME OVER')
+        isNextMovePossible = false;
+    }   
+};
