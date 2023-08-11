7203e536404e9c15594ac3fe69a2f808f08003bd
Kashish Kumar <kashishkumar.pc@gmail.com>
2023-08-11 23:10:20 +1000

    improved game, ADDED generate field functionality

--- generateField.js
+++ generateField.js
@@ -1,8 +1,31 @@
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+function generateField(rows, columns, percentOfHoles) {
+    const fieldCharacter = '░';
+    const maze = new Array(rows).fill(null).map(() => new Array(columns).fill(fieldCharacter));
+  
+    maze[0][0] = '*';
+  
+    const totalCells = rows * columns;
+    const numHoles = Math.floor((percentOfHoles / 100) * totalCells);
+  
+    let hatPlaced = false;
+    let holesPlaced = 0;
+  
+    while (hatPlaced === false || holesPlaced < numHoles) {
+      const randomRow = Math.floor(Math.random() * rows);
+      const randomColumn = Math.floor(Math.random() * columns);
+  
+      if (maze[randomRow][randomColumn] === fieldCharacter) {
+        if (!hatPlaced) {
+          maze[randomRow][randomColumn] = '^';
+          hatPlaced = true;
+        } else {
+          maze[randomRow][randomColumn] = 'O';
+          holesPlaced++;
+        }
+      }
+    }
+  
+    return maze;
+  };
 
-function generateField(height, width) {
-    
-};
+module.exports.generateField = generateField;
--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const prompt = require('prompt-sync')({sigint: true});
 const { Field } = require('./Field.js');
+const { generateField } = require('./generateField.js');
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+function playGame(myField) {
+    let isNextMovePossible = true;
+    while (isNextMovePossible) {
+
+        myField.print();
+        let move = prompt('Which way? ');
+
+        if (myField.isMoveWithinBoundary(move)) {
+
+            if (myField.elementAtNextPosition(move) === hole) {
+                console.log('You fell thru a hole, GAME OVER');
+                isNextMovePossible = false;
+            } else if (myField.elementAtNextPosition(move) === hat) {
+                console.log('You found the hat, GAME WON');
+                isNextMovePossible = false;
+            } else if (myField.elementAtNextPosition(move) === fieldCharacter) {
+                myField.updateField(myField.updatePosition(move));
+                console.clear();
+                isNextMovePossible = true;
+            } else {
+                console.log('ERROR');
+            }
+
+        } else {
+
+            console.log('You went out of the field, GAME OVER');
+            isNextMovePossible = false;
+        }
+    };
+}
 
 const myField = new Field([
     ['*', '░', 'O'],
@@ -12,33 +44,8 @@ const myField = new Field([
     ['░', '^', '░'],
   ]);
   
+const generatedField = new Field(generateField(15,15,20));
 
-let isNextMovePossible = true;
+playGame(generatedField);
 
-while (isNextMovePossible) {
 
-    myField.print();
-    let move = prompt('Which way? ');
-
-    if (myField.isMoveWithinBoundary(move)) {
-
-        if (myField.elementAtNextPosition(move) === hole){
-            console.log('You fell thru a hole, GAME OVER');
-            isNextMovePossible = false;
-        } else if (myField.elementAtNextPosition(move) === hat) {
-            console.log('You found the hat, GAME WON');
-            isNextMovePossible = false;
-        } else if (myField.elementAtNextPosition(move) === fieldCharacter) {
-            myField.updateField(myField.updatePosition(move));
-            console.clear();
-            isNextMovePossible = true;
-        } else {
-            console.log('ERROR');
-        }
-        
-    } else {
-
-        console.log('You went out of the field, GAME OVER')
-        isNextMovePossible = false;
-    }   
-};
